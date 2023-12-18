1f0c9a19c72ea0a46615645d53034134cadf709a
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2023-12-18 12:47:57 +0530

    Create Q3a.sh

---
+++ Q3a.sh
@@ -0,0 +1,16 @@
+# Develop a shell script that takes a valid directory name as an argument
+# and recursively descend all the sub-directories, finds the maximum
+# length of any file in that hierarchy, and store the output in a file.
+
+#! /bin/bash
+for i in $*
+do
+if [ -d $i ]
+then
+        echo "large filename size is&quot"
+
+        echo `ls -lR $1 | grep "^-" | tr -s ' ' | cut -d ' ' -f 5 | sort -n | tail -1`
+else
+echo "not directory"
+fi
+done
