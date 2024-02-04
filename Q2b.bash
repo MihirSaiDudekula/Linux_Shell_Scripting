c57df8c357a563ce8a090649aee2673c9bdbeb8d
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-02-04 12:23:02 +0530

    Update Q2b.bash

--- Q2b.bash
+++ Q2b.bash
@@ -2,14 +2,13 @@
 # &amp; display its creation time and permissions of the file, on the standard
 # output
 
-#! /bin/bash
-if [ $# -eq 0 ]
-then
-echo "display does not exit":
+#!/bin/bash
+if [ $# -eq 0 ]; then
+        echo "does not exist"
 else
-ls -l $1> t1
-x=`cut -d ' ' -f 1,6,7,8,9 t1`
-echo $x
+        ls -l "$1" > t1
+        x=$(cut -d ' ' -f 1,6,7,8,9 t1)
+        echo "$x"
 fi
 
 
