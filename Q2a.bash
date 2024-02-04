a454393e463520f1964f75733d2f82b78d76ca1f
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-02-04 11:23:43 +0530

    Update and rename Q2.bash to Q2a.bash

--- Q2a.bash
+++ Q2a.bash
@@ -17,19 +17,18 @@
 
 #!/bin/bash
 
-echo “ 10 levels of folders are created for the departments and 10 levels of files
-created for student details”
+echo "10 levels of folders are created for the departments and 10 levels of files created for student details"
+
 i=1
-while [ $i -le 10 ]
-do
-mkdir MSRITDept$i
-cd MSRITDept$i
-j=1
-while [ $j -le 20 ]
-do
-touch MSRITStudentDetails$j
-j=$(($j+1))
+while [ $i -le 10 ]; do
+    mkdir "MSRITDept$i"
+    cd "MSRITDept$i" || exit 1
+    j=1
+    while [ $j -le 20 ]; do
+        touch "MSRITStudentDetails$j"
+        ((j++))
+    done
+    cd ..
+    ((i++))
 done
-cd ..
-i=$(($i+1))
-done
+
