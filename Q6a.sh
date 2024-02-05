118e489ac2632dc8d75b9e699d9383c32e29ba10
Mihir Sai Dudekula <114400284+MihirSaiDudekula@users.noreply.github.com>
2024-02-05 12:11:14 +0530

    Create Q6a.sh

---
+++ Q6a.sh
@@ -0,0 +1,17 @@
+# Prog 6. a. Type the below text, Perform the following information by using sed.
+# “Python is a very popular language.
+# Python is easy to use. Python is easy to learn.
+# Python is a cross-platform language
+# HTML is a Markup Language
+# Python Programming Language
+# C Programming Language
+# Shell Programming
+# Perl Programming Language Bash”
+# i. Replace all instances of a Python in a second line of 1.txt with Perl.
+# ii. Replace the last occurrence of Programming with Scripting only of a match, not other instances.
+# iii. Create a text file in the path /MSRIT/CSE/UG/Python.txt. Replace full path with just the
+# filename no directory (such as Python.txt) and display it on standard output.
+# iv. Add string before and after the matching pattern using ‘\1’. In the above given text, navigate
+# yourself to last line, you can find Bash keyword, Add Learn before Bash and Programming after
+# Bash keyword
+
